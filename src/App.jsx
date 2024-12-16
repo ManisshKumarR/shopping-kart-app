5580822632f3eb2a85128936e3d167b167b3de5e
manish <manishkumar.r@aximsoft.com>
2024-12-16 23:12:20 +0530

    e-com-f

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import CartProvider from "./context/cart/CartProvider";
+import AppRoutes from "./routes/AppRoutes";
+const App = () => {
+  return (
+    <>
+      <CartProvider>
+        <AppRoutes />
+      </CartProvider>
+    </>
+  );
+};
+
+export default App;
